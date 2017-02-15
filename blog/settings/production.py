6afde29be9433582459ace35e381cf40373abbda
jameslin1235 <jameslin1235@gmail.com>
2017-02-14 19:24:30 -0800

    commit #15

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -125,8 +125,9 @@ STATICFILES_DIRS = (
 )
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Simplified static file serving.
