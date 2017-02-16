5dd077e46313c7119280a878b77670f081c9b9d0
jameslin1235 <jameslin1235@gmail.com>
2017-02-15 16:51:39 -0800

    commit #15

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
